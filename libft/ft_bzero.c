5c2c5b3da16e1c65cb3e6f53956e21002a94bbdc
Zoyerny <zoyern@gmail.com>
2023-10-31 00:26:41 +0100

    ft_bzero push

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almounib <almounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 23:55:33 by almounib          #+#    #+#             */
-/*   Updated: 2023/10/31 00:26:18 by almounib         ###   ########.fr       */
+/*   Created: 2023/10/31 00:19:46 by almounib          #+#    #+#             */
+/*   Updated: 2023/10/31 00:26:33 by almounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *s, int c, unsigned int n)
+void	ft_bzero(void *s, unsigned int n)
 {
 	unsigned char	*t;
 
 	t = (unsigned char)s;
 	while (n--)
-		*t++ = (unsigned char)c;
-	return (s);
+		*t++ = 0;
 }
