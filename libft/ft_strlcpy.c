a27f5e36e598b95db67f74dfc1a65f41db4727d1
Zoyerny <zoyern@gmail.com>
2023-11-04 18:12:46 +0100

    modified ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,21 +6,22 @@
 /*   By: zoyern <zoyern@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 01:00:16 by almounib          #+#    #+#             */
-/*   Updated: 2023/11/03 23:14:08 by zoyern           ###   ########.fr       */
+/*   Updated: 2023/11/04 18:11:49 by zoyern           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 unsigned long	ft_strlcpy(char *dst, const char *src, unsigned long sz)
 {
-	unsigned long	i;
+	unsigned long	len;
 
-	i = 0;
+	len = (ft_strlen(src));
 	if (!sz)
-		return (ft_strlen(src));
-	while (i++ < sz - 1)
+		return (len);
+	while (*src && sz-- - 1)
 		*dst++ = *src++;
-	dst[i] = '\0';
-	return (ft_strlen(src));
+	*dst = '\0';
+	return (len);
 }
